7f24fe19611327927e5a25e96c013d1a93f6c028
IceColdAV8R <76234749+IceColdAV8R@users.noreply.github.com>
2025-05-22 18:06:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,89 @@
+let textContent = '';
+const fltRls = {};
+//Regex
+
+var crewRgx = /(?<=\n)[A-Z]{2}:\s\d{6}\s(?:\w*\s?)*(?=\n)/gm;
+displayClock();
+setInterval(displayClock, 1000);
+
+function loadRelease() {
+  document.getElementById('inputText').style.display = 'none';
+  document.getElementById('submitButton').style.display = 'none';
+  var matchBox;
+  var loopHelp = true;
+  const textarea = document.getElementById('inputText');
+  textarea.value = textContent = textarea.value; // Store text in variable
+
+  var flightInformationRgx =
+    /FLIGHT\s(\d{4})\/\/([A-Z]{4})-([A-Z]{4})\/\/ETE\s(\d{2}:\d{2})/gm;
+  //1:Flight ID, 2:DEP, 3:ARR, 4:ETE
+  matchBox = flightInformationRgx.exec(textContent);
+  fltRls.ID = matchBox[1];
+  fltRls.DEP = matchBox[2];
+  fltRls.ARR = matchBox[3];
+  fltRls.ETE = matchBox[4];
+
+  var crewRgx = /(?<=\n)[A-Z]{2}:\s\d{6}\s(?:\w*\s?)*(?=\n)/gm;
+  fltRls.Crew = [];
+  while (loopHelp) {
+    matchBox = crewRgx.exec(textContent);
+    if (matchBox != null) {
+      fltRls.Crew.push(matchBox);
+    } else {
+      loopHelp = false;
+    }
+  }
+
+  var rlsNum = /RELEASE\sNO.\s\d{1,2}/gm;
+  fltRls.rlsNum = rlsNum.exec(textContent);
+  var aircraft = /(N\w{5})\s(E170.*)/gm; //1:Tail, 2:Type
+  fltRls.aircraft = aircraft.exec(textContent);
+  var authDep = /(?<=AUTHORIZED DATE\/TIME:\s)(\d{2}\w{3}\d{2})\s(\d{4}Z)/gm; //1:Date, 2:Time
+  matchBox = authDep.exec(textContent);
+  fltRls.SchedDate = matchBox[1];
+  fltRls.SchedTime = matchBox[2];
+  var skedDep = /(?<=SKED DEP DATE\/TIME:\s)(\d{2}\w{3}\d{2})\s(\d{4}Z)/gm; //1:Date, 2:Time
+  var skedArr = /(?<=SKED ARR DATE\/TIME:\s)(\d{2}\w{3}\d{2})\s(\d{4}Z)/gm; //1:Date, 2:Time
+  var payload = /PAYLOAD:\s(\d{1,5})\sPAYLOAD:\s(\d{1,5})/gm; //1:Planned, 2:Est Max
+  var pax = /PAX:\s(\d{1,2})\sPAX:\s(\d{1,2})/gm; //1:Planned, 2:Est Max
+  var bags = /BAGS:\s(\d{1,3})\sBAGS:\s(\d{1,3})/gm; //1:Planned, 2:Est Max
+
+  var MELs = /\w{2}-\w{2}-\w{2}-\w{1,2}\s.*/gm; // Try multiple matches
+  var CDLs = /\w{2}-\w{2}-\w{2}\s.*/gm; // Try multiple matches
+  var NEFs = /\w{2}-\w{2}-\w{3}.\s*/gm; // Try multiple matches
+  var pages =
+    /(?:REPUBLIC AIRWAYS BRIEF PAGE \d{1,2} OF \d{2})|(?:PAGE \d{1,2} OF \d{2})/g;
+
+  displayRelease();
+}
+
+function displayRelease() {
+  console.log(fltRls);
+  createChild('titleDiv', 'h1', "RPA "+fltRls.ID);
+  createChild('div1', 'h1',fltRls.rlsNum);
+  createChild('div1', 'h1', fltRls.aircraft[1] +" - - - "+ fltRls.aircraft[2]);
+  createChild('div1', 'h1', fltRls.DEP + ' - ' + fltRls.ARR);
+  createChild('div1', 'h1', "SKED DEP " + fltRls.SchedTime);
+  document.getElementById('div1').style.display = 'block';
+}
+
+function displayClock() {
+  const now = new Date();
+  const hours = String(now.getUTCHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const hoursL = String(now.getHours()).padStart(2, '0');
+  document.getElementById('clock').innerHTML = hours + minutes + 'Z   /   '+hoursL + minutes + "L";
+  
+}
+
+function createChild(parentId, childTag, innerHtml) {
+  const parent = document.getElementById(parentId);
+  if (!parent) {
+    console.error(`Parent element with ID "${parentId}" not found.`);
+    return null;
+  }
+  const child = document.createElement(childTag);
+  child.innerHTML = innerHtml;
+  parent.appendChild(child);
+}
 
