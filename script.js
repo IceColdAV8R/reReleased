ff3a5de37f65165e94170adc0032466a6b3adc2f
IceColdAV8R <76234749+IceColdAV8R@users.noreply.github.com>
2025-05-25 06:05:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const fltRls = {};
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('https://icecoldav8r.github.io/reReleased/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
       .then(registration => {
         console.log('Service Worker registered with scope:', registration.scope);
       })
