25a0d489decc161ed7462e23008c665002969b43
IceColdAV8R <76234749+IceColdAV8R@users.noreply.github.com>
2025-05-28 08:59:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -127,12 +127,12 @@ function loadRelease() {
   fltRls.rlsNum = rlsNum.exec(extractedText);
   var aircraft = /(N\w{5})\s(E170.*)/gm; //1:Tail, 2:Type
   fltRls.aircraft = aircraft.exec(extractedText);
-  var authDep = /(?<=AUTHORIZED DATE\/TIME:\s)(\d{2}\w{3}\d{2})\s(\d{4}Z)/gm; //1:Date, 2:Time
+  var authDep = /(?<=AUTHORIZED DATE\/TIME:\s)(\d{2}\w{3}\d{2})\s(\d{4}Z)/m; //1:Date, 2:Time
   matchBox = authDep.exec(extractedText);
   fltRls.AuthDep = [];
   fltRls.AuthDep.push(matchBox[1]);
   fltRls.AuthDep.push(matchBox[2]);
-  var skedDep = /(?<=SKED DEP DATE\/TIME:\s)(\d{2}\w{3}\d{2})\s(\d{4}Z)/gm; //1:Date, 2:Time
+  var skedDep = /(?<=SKED DEP DATE\/TIME:\s)(\d{2}\w{3}\d{2})\s(\d{4}Z)/m; //1:Date, 2:Time
   console.log(extractedText);
   matchBox = skedDep.exec(extractedText);
   fltRls.SkedDep = [];
